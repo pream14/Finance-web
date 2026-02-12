f46eb012a2c4dba273a059ef2d17b288bd5af8b9
pream14 <pream20041214@gmail.com>
2026-02-12 17:21:33 +0530

    apiview imported

--- backend/finance_app/transactions/views.py
+++ backend/finance_app/transactions/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Loan, Transaction
 from .serializers import LoanSerializer, LoanDetailSerializer, TransactionSerializer
 from customers.models import Customer
