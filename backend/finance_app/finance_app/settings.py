8ad07387e1029865ef60dc7aaad1807610361176
pream14 <pream20041214@gmail.com>
2026-03-10 09:32:43 +0530

    checking db

--- backend/finance_app/finance_app/settings.py
+++ backend/finance_app/finance_app/settings.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from pathlib import Path
-
+#checking railways db
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
