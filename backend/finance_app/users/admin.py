3aadf72d06800c639ded43ca84fc3c2e79809bbe
pream14 <pream20041214@gmail.com>
2026-05-12 11:27:51 +0530

    admin panel

--- backend/finance_app/users/admin.py
+++ backend/finance_app/users/admin.py
@@ -5,12 +5,12 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'phone_number', 'area', 'is_active')
-    list_filter = BaseUserAdmin.list_filter + ('role',)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_primary_owner', 'phone_number', 'area', 'is_active')
+    list_filter = BaseUserAdmin.list_filter + ('role', 'is_primary_owner')
     search_fields = BaseUserAdmin.search_fields + ('phone_number',)
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('Extra', {'fields': ('role', 'phone_number', 'area')}),
+        ('Extra', {'fields': ('role', 'is_primary_owner', 'phone_number', 'area')}),
     )
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ('Extra', {'fields': ('role', 'phone_number', 'area')}),
+        ('Extra', {'fields': ('role', 'is_primary_owner', 'phone_number', 'area')}),
     )
